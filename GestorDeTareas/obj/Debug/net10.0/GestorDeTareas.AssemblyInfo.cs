c87c110426b6ac79866fc9acb4fa5f234425acb0
Iker <162556194+IkerOwO@users.noreply.github.com>
2026-03-27 13:10:45 +0100

    Error solucionado 2

--- GestorDeTareas/obj/Debug/net10.0/GestorDeTareas.AssemblyInfo.cs
+++ GestorDeTareas/obj/Debug/net10.0/GestorDeTareas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorDeTareas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0093d94af2e1a28352d9570788d37add14f024eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9ea2925cfbb2f502f9f5f2ab3a7302624feda1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorDeTareas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorDeTareas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
