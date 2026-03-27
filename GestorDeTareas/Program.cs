7e2b2d3028c0b984d0b360e8f953b6d23c6f15ee
Iker <162556194+IkerOwO@users.noreply.github.com>
2026-03-27 11:07:48 +0100

    .gitignore agregado

--- GestorDeTareas/Program.cs
+++ GestorDeTareas/Program.cs
@@ -4,22 +4,81 @@ namespace GestorDeTareas
 {
     class Program
     {
-        private static string nombreArchivo { get; set; }
-        private static string user { get; set; }
-        
+        public class Archivo
+        {
+            public static string? nombreArchivo { get; set; }
+            public static string? user { get; set; }
+            public static string? ruta { get; set; }
+        }
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Introduce el usuario del PC: ");
-            user = Console.ReadLine();
+            Console.ResetColor();
+            Archivo.user = Console.ReadLine();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
             Console.WriteLine("Introduce el nombre del archivo: ");
-            nombreArchivo = Console.ReadLine();
+            Console.ResetColor();
+            Archivo.nombreArchivo = Console.ReadLine();
+
+            Archivo.ruta = $"C:\\Users\\{Archivo.user}\\Documents\\{Archivo.nombreArchivo}.txt";
+
+            CrearArchivo(Archivo.user, Archivo.nombreArchivo, Archivo.ruta);
+        }
+
+        static void CrearArchivo(string? usuario, string? nombreArchivo, string? ruta)
+        {
             try
             {
-                File.Create($"C:\\Users\\{user}\\Documents\\{nombreArchivo}.txt");
-            } catch (Exception e)
-            {
-                Console.WriteLine($"Error al crear el archivo: {e.Message}");
+                if (!File.Exists($"C:\\Users\\{usuario}\\Documents\\{nombreArchivo}.txt"))
+                {
+                    File.Create($"C:\\Users\\{usuario}\\Documents\\{nombreArchivo}.txt");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Archivo creado con exito");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("El archivo ya existe.");
+                    Console.ResetColor();
+                }
+                LeerArchivo(ruta);
+                Console.Clear();
             }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"ERROR AL CREAR EL ARCHIVO: {e.Message}");
+                Console.ResetColor();
+            }
+        }
+        
+        static void LeerArchivo(string? ruta)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Leyendo Archivo...");
+            Console.ResetColor();
+            Thread.Sleep(5000); // PAUSAR POR 5s
+            Console.Clear();
+
+            if(File.Exists(ruta))
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("---------CONTENIDO DEL ARCHIVO---------\n");
+                Console.ResetColor();
+                // USAMOS StreamReader PORQUE LEE TODAS LAS LINEAS Y LAS IMPRIME TAL CUAL ESTAN ESCRITAS
+                using (StreamReader sr = File.OpenText(ruta))
+                {
+                    string s;
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+
         }
     }
 }
