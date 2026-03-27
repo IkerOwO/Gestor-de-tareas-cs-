9faf1f15eaac27f054d0283a9effb41561b39f89
Iker <162556194+IkerOwO@users.noreply.github.com>
2026-03-27 10:45:21 +0100

    Initial Commit

---
+++ GestorDeTareas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace GestorDeTareas
+{
+    class Program
+    {
+        private static string nombreArchivo { get; set; }
+        private static string user { get; set; }
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el usuario del PC: ");
+            user = Console.ReadLine();
+            Console.WriteLine("Introduce el nombre del archivo: ");
+            nombreArchivo = Console.ReadLine();
+            try
+            {
+                File.Create($"C:\\Users\\{user}\\Documents\\{nombreArchivo}.txt");
+            } catch (Exception e)
+            {
+                Console.WriteLine($"Error al crear el archivo: {e.Message}");
+            }
+        }
+    }
+}
